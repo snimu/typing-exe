d8727a87669373ebca3b29648bad04d2884a8212
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2022-12-20 13:57:14 +0100

    fix annotations.pyi

--- src/parameter_checks/annotations.pyi
+++ src/parameter_checks/annotations.pyi
@@ -1,6 +1,16 @@
-class _ItemCreator:
+class _ChecksCreator:
+    def __init__(self):
+        self.checks = None
+        self.typehint = None
+
     def __getitem__(self, item): ...
 
 
-Checks: _ItemCreator
-Hook: _ItemCreator
+class _HookCreator:
+    def __init__(self): ...
+
+    def __getitem__(self, item): ...
+
+
+Checks: _ChecksCreator
+Hook: _HookCreator
