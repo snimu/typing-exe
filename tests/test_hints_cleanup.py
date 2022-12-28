f8e13e84827775164317fdecfed9b52796129a61
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2022-12-28 13:31:45 +0100

    test that `cleanup_annotations` works with strongtyping package

--- tests/test_hints_cleanup.py
+++ tests/test_hints_cleanup.py
@@ -1,6 +1,9 @@
 from typing import Union
 import pytest
 
+from strongtyping.strong_typing import match_typing
+from strongtyping.strong_typing_utils import TypeMisMatch
+
 from typing_exe.annotations import Assert, Sequence, Modify
 from typing_exe.decorators import execute_annotations, cleanup_annotations
 
@@ -36,3 +39,27 @@ def test_cleanup():
 
     # Check that annotations are fine
     assert fct.__annotations__ == {"b": float, "c": int, "e": Union[int, float], "f": int, "return": int}
+    
+    
+def test_with_strongtyping():
+    @match_typing
+    @cleanup_annotations
+    @execute_annotations
+    def fct(
+            a: Assert[lambda a: a > 0],
+            b: float,
+            c: Assert[int, lambda c: c != 0],
+            d,
+            e: Union[int, float],
+            f: Sequence[
+                int,
+                Assert[lambda a: a != 0],
+                Modify[lambda a: a + 1]
+            ]
+    ) -> Modify[int]:
+        return int(a + b + c + d + e + f)
+
+    assert fct(1, 1., 1, 1, 1, 1) == 7
+
+    with pytest.raises(TypeMisMatch):
+        fct(1, 1, 1, 1, 1, 1, 1)
